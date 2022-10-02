8986b4bb7d262a1b9df78ce6b7de058d835f63d9
vavpan <v4projectvps@gmail.com>
2022-10-02 19:56:46 +0300

    Weather description has been added

--- src/main/java/com/example/Weather/Api/controller/WeatherController.java
+++ src/main/java/com/example/Weather/Api/controller/WeatherController.java
@@ -1,5 +1,6 @@
 package com.example.Weather.Api.controller;
 
+import com.example.Weather.Api.details.WeatherDetails;
 import com.example.Weather.Api.model.WeatherModel;
 import com.example.Weather.Api.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +20,21 @@ public class WeatherController {
     @GetMapping("/info/{cityValue}")
     private WeatherModel getWeatherByCity(@PathVariable("cityValue") String cityValue){
 
-        System.out.println(weatherService.getWeatherInfoByCity(cityValue).getName());
-        System.out.println(weatherService.getWeatherInfoByCity(cityValue).getMain().getTemp());
+        String name = weatherService.getWeatherInfoByCity(cityValue).getName();
+        double temp =  weatherService.getWeatherInfoByCity(cityValue).getMain().getTemp();
+        String desc = weatherService.getWeatherInfoByCity(cityValue).getWeather().listIterator().next().getDescription();
+        double minTemp = weatherService.getWeatherInfoByCity(cityValue).getMain().getTemp_min();
+        double maxTemp = weatherService.getWeatherInfoByCity(cityValue).getMain().getTemp_max();
+
+        System.out.println("City: " +name+  "\nCurrent temperature: " +temp+ "\nDescription: " +desc+ "\nToday minimum temperature: " +minTemp+ "\nToday maximum temperature: " +maxTemp);
+        System.out.println("=====\n");
+
+
         return weatherService.getWeatherInfoByCity(cityValue);
 
 
     }
+
 
 
 }
